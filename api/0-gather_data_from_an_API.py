544669b88f11fee64ab976207c3d3dee3a4e92ee
lenne sebastien <sebastienlenne@gmail.com>
2023-08-21 11:16:58 +0200

    update file for task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,28 +1,39 @@
 #!/usr/bin/python3
 """Script that prints specific information from an API"""
-
 import json
 import requests
 import sys
 
-
 if __name__ == '__main__':
     API_URL = 'https://jsonplaceholder.typicode.com'
 
-    id = sys.argv[2]
-    request = requests.get('{}/users/{}/todos'.format
-                           (API_URL, id), params={"_expand": "user"})
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+
+    request = requests.get(f'{API_URL}/users/{employee_id}/todos', params={"_expand": "user"})
+
+    if request.status_code != 200:
+        print(f"Failed to fetch data for employee ID {employee_id}")
+        sys.exit(1)
 
     response = request.json()
 
+    if not response:
+        print(f"No data found for employee ID {employee_id}")
+        sys.exit(1)
+
     completed_tasks = [task for task in response if task['completed']]
-    EMPLOYEE_NAME = response[0]['user']['name']
-    NUMBER_OF_DONE_TASKS = len(completed_tasks)
-    TOTAL_NUMBER_OF_TASKS = len(response)
+    if response:
+        EMPLOYEE_NAME = response[0]['user']['name']
+        NUMBER_OF_DONE_TASKS = len(completed_tasks)
+        TOTAL_NUMBER_OF_TASKS = len(response)
 
-    print("Employee {} is done with tasks({}/{}):".format(
-        EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS
-    ))
+        print("Employee {} is done with tasks({}/{}):".format(
+            EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS
+        ))
 
-    for task in completed_tasks:
-        print("\t {}".format(task['title']))
+        for task in completed_tasks:
+            print("\t {}".format(task['title']))
