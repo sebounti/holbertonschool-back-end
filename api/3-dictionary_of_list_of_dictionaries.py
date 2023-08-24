7d8c5f63504e3e5f0379ec84d9244291a0b9740a
lenne sebastien <sebastienlenne@gmail.com>
2023-08-24 22:15:13 +0200

    update task 3 project api

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,32 +1,29 @@
 #!/usr/bin/python3
-"""Export data in the JSON format"""
+"""Print a user and their completed tasks"""
 import json
 import requests
-from sys import argv
+import sys
 
 
-def get_api():
-    """gather data from an API"""
-    url = 'https://jsonplaceholder.typicode.com/'
-    uid = argv[1]
+if __name__ == "__main__":
+    user_url = "https://jsonplaceholder.typicode.com/users"
+    user = requests.get(user_url)
 
-    # get a specific user from users in jsonplaceholder
-    usr = requests.get(url + 'users/{}'.format(uid)).json()
-    # make a query string to get tasks based on user id
-    todo = requests.get(url + 'todos', params={'userId': uid}).json()
+    user_json = user.json()
 
-    with open('{}.json'.format(uid), 'w') as file:
-        obj = {uid: []}
-        for employee in todo:
-            tmp_obj = {
-                    'task': employee.get('title'),
-                    'completed': employee.get('completed'),
-                    'username': usr.get('username')
-                    }
-            obj[uid].append(tmp_obj)
-        # serialize object into a JSON string
-        json.dump(obj, file)
+    all_dicts = {
+            }
 
+    for i in user_json:
+        todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos"\
+                   .format(i['id'])
+        todos = requests.get(todo_url)
+        todo = todos.json()
+        all_dicts[i['id']] = []
+        for j in range(0, len(todo)):
+            all_dicts[i['id']].append({"username": i['username'],
+                                       "task": todo[j]["title"],
+                                       "completed": todo[j]["completed"]})
 
-if __name__ == '__main__':
-    get_api()
+    with open("todo_all_employees.json", 'w') as f:
+        f.write(json.dumps(all_dicts))
